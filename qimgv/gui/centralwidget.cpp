f126d79d93b4328089a80f76bf36997447bbebb7
hadoooooouken <42491193+hadoooooouken@users.noreply.github.com>
2026-06-19 03:02:53 +0300

    ui: add smooth cross-fade transition between viewer and folderview

--- qimgv/gui/centralwidget.cpp
+++ qimgv/gui/centralwidget.cpp
@@ -1,5 +1,9 @@
 #include "centralwidget.h"
 #include "settings.h"
+#include <QLabel>
+#include <QGraphicsOpacityEffect>
+#include <QPropertyAnimation>
+#include <QResizeEvent>
 
 CentralWidget::CentralWidget(std::shared_ptr<DocumentWidget> _docWidget, std::shared_ptr<FolderViewProxy> _folderView, QWidget *parent)
     : QStackedWidget(parent),
@@ -28,8 +32,7 @@ void CentralWidget::showDocumentView() {
     if(mode == MODE_DOCUMENT)
         return;
     mode = MODE_DOCUMENT;
-    setCurrentIndex(0);
-    widget(0)->setFocus();
+    switchView(0);
 }
 
 void CentralWidget::showFolderView() {
@@ -37,9 +40,7 @@ void CentralWidget::showFolderView() {
         return;
 
     mode = MODE_FOLDERVIEW;
-    setCurrentIndex(1);
-    widget(1)->show();
-    widget(1)->setFocus();
+    switchView(1);
 }
 
 void CentralWidget::toggleViewMode() {
@@ -48,4 +49,61 @@ void CentralWidget::toggleViewMode() {
 
 ViewMode CentralWidget::currentViewMode() {
     return mode;
+}
+
+void CentralWidget::switchView(int index) {
+    if (isVisible()) {
+        QPixmap oldPixmap = grab();
+
+        if (overlayLabel) {
+            delete overlayLabel;
+        }
+
+        setUpdatesEnabled(false);
+
+        overlayLabel = new QLabel(this);
+        overlayLabel->setAttribute(Qt::WA_TransparentForMouseEvents);
+        overlayLabel->setPixmap(oldPixmap);
+        overlayLabel->setGeometry(rect());
+        overlayLabel->show();
+
+        setCurrentIndex(index);
+        QWidget *w = widget(index);
+        if (w) {
+            w->show();
+            w->setFocus();
+        }
+
+        overlayLabel->raise();
+
+        setUpdatesEnabled(true);
+        overlayLabel->repaint();
+
+        QGraphicsOpacityEffect *effect = new QGraphicsOpacityEffect(overlayLabel);
+        effect->setOpacity(1.0);
+        overlayLabel->setGraphicsEffect(effect);
+
+        QPropertyAnimation *animation = new QPropertyAnimation(effect, "opacity", overlayLabel);
+        animation->setDuration(160);
+        animation->setStartValue(1.0);
+        animation->setEndValue(0.0);
+        animation->setEasingCurve(QEasingCurve::OutQuad);
+        connect(animation, &QPropertyAnimation::finished, overlayLabel, &QLabel::deleteLater);
+
+        animation->start(QAbstractAnimation::DeleteWhenStopped);
+    } else {
+        setCurrentIndex(index);
+        QWidget *w = widget(index);
+        if (w) {
+            w->show();
+            w->setFocus();
+        }
+    }
+}
+
+void CentralWidget::resizeEvent(QResizeEvent *event) {
+    QStackedWidget::resizeEvent(event);
+    if (overlayLabel) {
+        overlayLabel->setGeometry(rect());
+    }
 }
--- qimgv/gui/centralwidget.h
+++ qimgv/gui/centralwidget.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <QStackedWidget>
+#include <QPointer>
 #include "gui/folderview/folderviewproxy.h"
 #include "gui/viewers/documentwidget.h"
 #include "settings_types.h"
 
+class QLabel;
+class QResizeEvent;
 
 class CentralWidget : public QStackedWidget
 {
@@ -20,8 +23,14 @@ public slots:
     void showFolderView();
     void toggleViewMode();
 
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
 private:
+    void switchView(int index);
+
     std::shared_ptr<DocumentWidget> documentView;
     std::shared_ptr<FolderViewProxy> folderView;
     ViewMode mode;
+    QPointer<QLabel> overlayLabel;
 };
